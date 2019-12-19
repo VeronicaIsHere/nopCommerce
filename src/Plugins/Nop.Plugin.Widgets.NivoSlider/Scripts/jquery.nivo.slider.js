bd2ac528ccb1f8064cd0e77d572e0960e6e0a4e0
Veronica <veronika.s@nopCommerce.com>
2019-12-19 12:14:56 +0300

    Revert "Changed child size in nivoSlider" what now

--- src/Plugins/Nop.Plugin.Widgets.NivoSlider/Scripts/jquery.nivo.slider.js
+++ src/Plugins/Nop.Plugin.Widgets.NivoSlider/Scripts/jquery.nivo.slider.js
@@ -40,7 +40,7 @@
                 child = child.find('img:first');
             }
             // Get img width & height
-            var childWidth = 25,
+            var childWidth = (childWidth === 0) ? child.attr('width') : child.width(),
                 childHeight = (childHeight === 0) ? child.attr('height') : child.height();
 
             if (link !== '') {
